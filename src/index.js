37c205c510afe2b95d88753718af2f880596aa84
vickyvikraman <vigramanmoorthy@gmail.com>
2019-05-14 18:26:40 +0530

    file change for docker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Routing from './route'
-
 ReactDOM.render(<Routing />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- src/route.js
+++ src/route.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as  Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import Room from './room'
 import Main from './main'
@@ -12,7 +12,7 @@ class Routing extends Component {
     return (
         <BrowserRouter>
           <div className="App">
-            <Route exact path ="/ "  component={() => <Home />}/> 
+            <Route exact path ="/"  component={() => <Home />}/> 
             <Route exact path ="/channel"  component={() => <Main />}/>
             <Route exact path ="/room"  component={() => <Room />}/>
           </div>
